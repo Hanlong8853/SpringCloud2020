64219810dd95d3cf18d64f3b672f132f0ec1c7bf
hanlong <305088hanlong@163.com>
2020-04-16 10:30:34 +0800

    001

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/MyselfRule/MySelfRule.java
@@ -0,0 +1,26 @@
+/**
+ *  Copyright (C), 2015-2020
+ * FileName: MySelfRule
+ * Author: hanlong
+ * Date: 2020/4/16 10:19 上午
+ * Description:
+ */
+package com.atguigu.MyselfRule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Configuration;
+
+/** Copyright (C), 2015-2020 
+ * FileName: MySelfRule 
+ * Author: hanlong 
+ * Date: 2020/4/16 10:19 上午 
+ *Description:  自动以负载均衡规则类
+ */
+@Configuration
+public class MySelfRule {
+    public IRule myRule()
+    {
+        return new RandomRule();
+
+    }}
